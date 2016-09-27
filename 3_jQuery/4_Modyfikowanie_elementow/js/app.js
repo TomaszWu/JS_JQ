ad23121b2b105111b8c1e8ed3680dadeb5349002
TomaszWu <tomasz.wojciechowski84@gmail.com>
2016-09-27 21:59:57 +0200

    modification

--- 3_jQuery/4_Modyfikowanie_elementow/js/app.js
+++ 3_jQuery/4_Modyfikowanie_elementow/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     
     
-    var btn = $('input[type=submit]'); // button
+    var btn = $('input[type=submit]'); // button nawias bo to jest selektor
     var inputUser = $('#addUser');
     var inputAge = $('#age');
     var ul = $('ul.main');
@@ -24,4 +24,12 @@ $(document).ready(function () {
         }
         ul.append(newLi);
     });
+    
+    
+    console.log('ZADANIE 2 ____________________');
+    
+    
+    $('.where-i-am div').append('<span>1.Jestem tutaj...</span>').prepend('<span>2.Jestem tutaj...</span>').parent().prepend('<span>3.Jestem tutaj...</span>').append('<span>4.Jestem tutaj...</span>');
+    
+    
 });
