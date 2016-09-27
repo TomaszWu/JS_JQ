70803f1bb161f5316d9e32c88a9621016de24816
TomaszWu <tomasz.wojciechowski84@gmail.com>
2016-09-27 21:59:34 +0200

    eventy

--- 3_jQuery/3_Eventy/js/app.js
+++ 3_jQuery/3_Eventy/js/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    
+
     var btns = $('div.hero-buttons').find('button');
-    
+
     var btnIronMan = btns.eq(0);
     var btnThor = btns.eq(1);
     var btnWolverin = btns.eq(2);
     console.log(btns);
-    
+
 //    btnIronMan.on('click', function(){
 //        console.log('Kliknięto mnie');
 //    });
@@ -19,42 +19,66 @@ $(document).ready(function () {
 //    });
 //    
     console.log('zadanie2______________________________________-');
-    
+
     var dd = $('section.superhero-description').find('dd').hide();
-    
-   
+
+
     var dt = $('section.superhero-description').find('dt');
-    
-    dt.on('click', function(){
-        
+
+    dt.on('click', function () {
+
         $(this).next().slideToggle();
-        
+
     });
-    
-    
-    
+    console.log('zadanie3______________________________________-');
+
+
+
+
+
+
+
     console.log('zadanie4______________________________________-');
-    
-    
-    $('.show-hide-btn').on('click', function(event){
+
+
+    $('.show-hide-btn').on('click', function (event) {
         event.preventDefault();
         var input = $('.user-pass');
-        if(input.attr('type') == 'password'){
+        if (input.attr('type') == 'password') {
             input.attr('type', 'text');
         } else {
             input.attr('type', 'password');
         }
     });
-    
+
 //    http://wklej.to/oqPiw
-    
-    
+
+
     console.log('zadanie5______________________________________-');
-    
-    
-    $('.menu').on('mouseenter', function() {
+
+
+    $('.menu').on('mouseenter', function () {
         console.log('Hurra!!!');
-        
+
     });
-    
+
+    console.log('zadanie5______________________________________-');
+
+
+    var inputs = $('.Login').find('input');
+
+
+    inputs.on('focus', function () {
+        for (var i = 0; i < inputs.length; i++) {
+            $(this).css('background-color', 'lightGray');
+        }
+    });
+
+    inputs.on('mouseleave', function () {
+        for (var i = 0; i < inputs.length; i++) {
+            $(this).css('background-color', 'gray');
+        }
+
+    });
+
 });
