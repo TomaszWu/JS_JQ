da38692c28adb0aa093165c9696e6d5795fb42a5
TomaszWu <tomasz.wojciechowski84@gmail.com>
2016-10-02 17:39:42 +0200

    exercie2

--- 2_DOM/7_Inputy_i_formularze/js/zadanie02.js
+++ 2_DOM/7_Inputy_i_formularze/js/zadanie02.js
@@ -2,3 +2,37 @@
  * Created by Jacek on 2016-01-12.
  */
 // zrobić funkcję na elementach z ceną. Po dodaniu jakiegoś elementu czyli kliknięcu funkcja od nowa sprawdza wszystko.
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var checkBoxIfChecked = document.querySelector('input#invoice');
+    var divToHide = document.querySelector('div#invoiceData');
+    var form = document.querySelector('form');
+    var beforeThisPAddADiv = document.querySelector('form p');
+    
+    checkBoxIfChecked.addEventListener('click', function () {
+        var sumOfElements = document.querySelector('form').children.length;
+        if (sumOfElements == 5) {
+            divToHide.parentNode.removeChild(divToHide);
+        } else {
+            form.insertBefore(divToHide, beforeThisPAddADiv);
+        }
+    });
+    
+    
+    
+    
+    
+    
+//    checkBoxIfChecked.addEventListener('click', function () {
+//        if (divToHide.style.visibility === 'hidden') {
+//            divToHide.style.visibility = 'visible';
+//        } else {
+//            divToHide.style.visibility = 'hidden';
+//        }
+//    });
+
+
+
+});
