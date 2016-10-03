0e410bee5ff4c63b827d2475ebe038959152e10f
TomaszWu <tomasz.wojciechowski84@gmail.com>
2016-10-03 20:45:50 +0200

    exercise2

--- 2_DOM/7_Inputy_i_formularze/js/zadanie04.js
+++ 2_DOM/7_Inputy_i_formularze/js/zadanie04.js
@@ -1,3 +1,49 @@
 /**
  * Created by Jacek on 2016-01-12.
  */
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+
+    var divToHide = document.querySelectorAll('.page-header img');
+
+    function displayNoneAll() {
+        for (var i = 0; i < divToHide.length; i++) {
+            divToHide[i].style.visibility = 'hidden';
+
+        }
+    }
+
+    displayNoneAll();
+
+// przy wybrani Windows nic się nie pojawia, dopiero po wyborze którejś opcji z kolei działa. 
+
+    var options = document.querySelector('.form-control');
+    options.addEventListener('change', function () {
+        event.preventDefault();
+        console.log(options.value);
+        switch (options.value) {
+            case('Windows'):
+                displayNoneAll();
+                divToHide[2].style.visibility = 'visible';
+                break;
+            case('Os X'):
+                displayNoneAll();
+                divToHide[0].style.visibility = 'visible';
+                break;
+            case('Ubuntu'):
+                displayNoneAll();
+                divToHide[1].style.visibility = 'visible';
+                break;
+        }
+        ;
+    });
+
+
+});
+
+
+
+
+
