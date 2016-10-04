20b655abaa041361ce605176f5c1d6799cc3830a
TomaszWu <tomasz.wojciechowski84@gmail.com>
2016-10-04 19:43:03 +0200

    gallery

--- Zadania_dodatkowe_JavaScript/2_Gallery/js/app.js
+++ Zadania_dodatkowe_JavaScript/2_Gallery/js/app.js
@@ -4,7 +4,46 @@
 
 /*
  <div class="fullScreen">
-   <img src="./images/wrong.png">
-   <button class="close">Close</button>
+ <img src="./images/wrong.png">
+ <button class="close">Close</button>
  </div>
  */
+
+document.addEventListener('DOMContentLoaded', function () {
+    var ul = document.querySelector('ul');
+    ul.classList.add('gallery');
+
+
+
+    var allLi = document.querySelectorAll('ul.gallery li');
+    var body = document.querySelector('body');
+
+    for (var i = 0; i < allLi.length; i++) {
+        allLi[i].addEventListener('click', function () {
+            var test = this.querySelector('img').src;
+
+            var newDiv = document.createElement('div');
+            var newImg = document.createElement('img');
+            var newButton = document.createElement('button');
+            newImg.src = test;
+            body.appendChild(newDiv);
+            newDiv.classList.add('fullScreen');
+            newDiv.appendChild(newImg);
+            newDiv.appendChild(newButton);
+            newButton.classList.add('close');
+            newButton.innerHTML = 'Close';
+            var newButton = document.querySelector('button.close');
+            newButton.addEventListener('click', function () {
+                newDiv.parentNode.removeChild(newDiv);
+
+            });
+
+        });
+    }
+
+
+
+
+
+
+});
