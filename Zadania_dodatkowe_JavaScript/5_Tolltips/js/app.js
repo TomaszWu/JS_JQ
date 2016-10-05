01d011fd0f5e594ab1acdd5ae977a905e0de7bb0
TomaszWu <tomasz.wojciechowski84@gmail.com>
2016-10-05 21:58:23 +0200

    tooltip

--- Zadania_dodatkowe_JavaScript/5_Tolltips/js/app.js
+++ Zadania_dodatkowe_JavaScript/5_Tolltips/js/app.js
@@ -6,3 +6,23 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener('DOMContentLoaded', function(){
+    
+    
+    var tooltips = document.getElementsByClassName('tooltip');
+    
+    for(var i = 0; i < tooltips.length; i++){
+        tooltips[i].addEventListener('mouseover', function() {
+            var newSpan = document.createElement('span');
+            newSpan.innerHTML = this.dataset.text;
+            newSpan.classList.add('tooltipText');
+            this.appendChild(newSpan);
+        });
+        tooltips[i].addEventListener('mouseout', function() {
+            var spanToDelete = this.firstElementChild;
+            spanToDelete.parentNode.removeChild(spanToDelete);
+            
+        });
+        
+    }
+});
