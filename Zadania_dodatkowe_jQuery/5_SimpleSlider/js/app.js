a8439eb8e73968c5afd95b4ad32b8bda8a16cbb2
TomaszWu <tomasz.wojciechowski84@gmail.com>
2016-10-07 20:15:23 +0200

    failureSoFar

--- Zadania_dodatkowe_jQuery/5_SimpleSlider/js/app.js
+++ Zadania_dodatkowe_jQuery/5_SimpleSlider/js/app.js
@@ -1,36 +1,28 @@
-$(function(){
+$(function () {
 
 
-    $('button.changeBar:nth-of-type(3n+1').data('data-width', 0.5).data('data-color', 'blue');
-    $('button.changeBar:nth-of-type(3n+2').data('data-width', 0.2).data('data-color', 'green');
-    $('button.changeBar:nth-of-type(3n+3').data('data-width', 0.9).data('data-color', 'orange');
-    $('div#bar1').prevAll().data('data-bar', 1);
-    $('div#bar2').prevUntil('div').data('data-bar', 2);
-    $('div#bar3').prevUntil('div').data('data-bar', 3);
-    
-    
-    
-    var buttons = $('button.changeBar');
-    var divs = $('div.progress-bar');
-    
-    buttons.on('click', function(){
-        var width = $(this).data('data-width');
-        var widthToSet = width * 350 + 'px';
-        
-        var bar = $(this).data('data-bar');
-        var color = $(this).data('data-color');
-        console.log(widthToSet);
-        console.log(bar);
-        console.log(color);
-        var span = $('#bar' + bar + ' span');
-        
-        span.removeClass();
-        span.addClass(color);
-        span.animate({
-            width: widthToSet
-        });
-        
-        
+    var btnNext = $('button#nextPicture');
+    var btnPrv = $('button#prevPicture');
+    var allLi = $('.slider li');
+    var index = 0;
+    var width = 400;
+    $('ul').css('width', 2500 + 'px').css('position', 'relative');
+
+// póki co bez sukcesów. Prosiłbym o pomoc. 
+
+
+    btnNext.on('click', function () {
+        index += 1;
+        if (index > 5) {
+            index = 0;
+        }
+        $(allLi[index]).animate({"left": '+=200' + 'px'});
     });
-    
+    btnPrv.on('click', function () {
+        index += 1;
+        if (index > 5) {
+            index = 0;
+        }
+    });
+
 });
